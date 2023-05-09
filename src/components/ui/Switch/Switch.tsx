524afe16c8b656f06b8c98120f94f4f3ceaca82c
xStrixU <strixu155@gmail.com>
2023-05-09 17:55:37 +0200

    fix: fix ref errors

--- src/components/ui/Switch/Switch.tsx
+++ src/components/ui/Switch/Switch.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 type SwitchProps = Readonly<{
@@ -6,16 +7,21 @@ type SwitchProps = Readonly<{
 	'aria-label'?: string;
 }>;
 
-export const Switch = ({ checked, ...props }: SwitchProps) => (
-	<input
-		type="checkbox"
-		role="switch"
-		aria-checked={checked}
-		checked={checked}
-		className={twMerge(
-			'relative h-6 w-10 cursor-pointer appearance-none rounded-full bg-gray-300 transition-colors before:absolute before:left-1 before:top-1 before:h-[16px] before:w-[16px] before:rounded-full before:bg-white before:transition-all before:duration-200 [&:checked]:before:left-[calc(100%-0.25rem)] [&:checked]:before:-translate-x-full',
-			checked && 'bg-primary'
-		)}
-		{...props}
-	/>
+export const Switch = forwardRef<HTMLInputElement, SwitchProps>(
+	({ checked, ...props }, ref) => (
+		<input
+			type="checkbox"
+			role="switch"
+			ref={ref}
+			aria-checked={checked}
+			checked={checked}
+			className={twMerge(
+				'relative h-6 w-10 cursor-pointer appearance-none rounded-full bg-gray-300 transition-colors before:absolute before:left-1 before:top-1 before:h-[16px] before:w-[16px] before:rounded-full before:bg-white before:transition-all before:duration-200 [&:checked]:before:left-[calc(100%-0.25rem)] [&:checked]:before:-translate-x-full',
+				checked && 'bg-primary'
+			)}
+			{...props}
+		/>
+	)
 );
+
+Switch.displayName = 'Switch';
